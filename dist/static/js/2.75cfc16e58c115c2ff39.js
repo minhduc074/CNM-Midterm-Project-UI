4246a3feff4bb307f86f0d1e16ddb35e2b3dcb88
bminhduc <minhduc074@gmail.com>
2018-11-07 09:55:31 +0700

    deploy heroku

---
+++ dist/static/js/2.75cfc16e58c115c2ff39.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{ee35:function(e,t,s){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"hello"},[t("h1",[this._v("The Staff pages")])])}]},l=s("VU/8")({},n,!1,null,null,null);t.default=l.exports}});
+//# sourceMappingURL=2.75cfc16e58c115c2ff39.js.map
